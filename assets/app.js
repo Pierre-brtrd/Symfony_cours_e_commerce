d84750ad34781ceb2b00bd69bf8633a4b056e41e
pierre.bertrand.professionnel <pierre.bertrand.professionnel@gmail.com>
2024-03-05 16:41:22 +0100

    feat(webpack): install and configure webpack

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,4 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
-
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+import './styles/app.scss';
--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
