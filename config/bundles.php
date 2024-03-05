161e1f54f5d26ba0f1922710a88e45e432676c9e
pierre.bertrand.professionnel <pierre.bertrand.professionnel@gmail.com>
2024-03-05 16:18:59 +0100

    feat(init): add bundle vich and stof doctrine  and sanitizer

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
