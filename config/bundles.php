53f98aeb810c550e506095109b4b27c606d8ce5c
pierre.bertrand.professionnel <pierre.bertrand.professionnel@gmail.com>
2024-03-06 08:32:48 +0100

    feat(users): add user model and user constraints

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
